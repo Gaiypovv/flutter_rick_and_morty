89abf1cc4587176eeb17f4cf8f9c5b9851db6f30
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 12:09:34 +0600

    указал файлы где будут генерироваться весь наш код

--- lib/bloc/character_bloc.dart
+++ lib/bloc/character_bloc.dart
@@ -1,1 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'character_bloc.freezed.dart';
+part 'character_bloc.g.dart';
+part 'character_event.dart'
+part 'character_state.dart'
