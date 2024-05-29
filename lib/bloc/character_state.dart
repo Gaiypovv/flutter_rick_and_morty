7b080e85a2e5d85ec99e97e9bcda7a825b57969d
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 12:58:28 +0600

    добавил состояние ошибки

--- lib/bloc/character_state.dart
+++ lib/bloc/character_state.dart
@@ -5,4 +5,5 @@ class CharacterState with _$CharacterState {
   const factory CharacterState.loading() = CharacterStateLoading;
   const factory CharacterState.loaded({required Character characterLoaded}) =
       CharacterStateLoading;
+  const factory CharacterState.error() = CharacterStateError;
 }
