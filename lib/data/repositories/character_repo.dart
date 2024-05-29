0be7dfcdfe27179aebde4a4bff16d78a3700e38f
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 11:52:06 +0600

    добавил правильно http пакет и реализовал запрос

--- lib/data/repositories/character_repo.dart
+++ lib/data/repositories/character_repo.dart
@@ -1,1 +1,10 @@
-class CharacterRepo {}
+import 'package:flutter_rick_and_morty/data/models/character.dart';
+import 'package:http/http.dart' as http;
+
+class CharacterRepo {
+  final url = 'https://rickandmortyapi.com/api/character';
+
+  Future<Character> getCharacter(int page, String name) async {
+    var response = await http.get(Uri.parse(url + '?page=$page&name=$name'));
+  }
+}
