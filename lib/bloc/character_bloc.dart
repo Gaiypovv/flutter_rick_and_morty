156dcafabf3873cf41fa923da0b1b8ea8b84b4be
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 13:05:01 +0600

    закоментил character_bloc

--- lib/bloc/character_bloc.dart
+++ lib/bloc/character_bloc.dart
@@ -2,6 +2,8 @@ import 'package:flutter_rick_and_morty/data/models/character.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'character_bloc.freezed.dart';
-part 'character_bloc.g.dart';
+// part 'character_bloc.g.dart';
 part 'character_event.dart';
 part 'character_state.dart';
+
+class CharacterBloc extends Bloc
