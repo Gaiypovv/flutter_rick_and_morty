142b0236fd66c0017eec9f9ca0687a4f37360e1a
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 15:18:05 +0600

    Добавлен класс HomePage с заглушкой и инициализацией CharacterRepo. Теперь HomePage принимает title и создает экземпляр CharacterRepo.

--- lib/UI/pages/home_page.dart
+++ lib/UI/pages/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rick_and_morty/data/repositories/character_repo.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  final repository = CharacterRepo();
 
   @override
   Widget build(BuildContext context) {
