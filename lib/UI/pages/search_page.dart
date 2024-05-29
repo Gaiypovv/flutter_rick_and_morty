ed72bd394698dffbf16d925b52ac1e2d85f13224
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 15:52:09 +0600

    Вызов метода fetch в initState для инициализации загрузки персонажей при запуске страницы

--- lib/UI/pages/search_page.dart
+++ lib/UI/pages/search_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rick_and_morty/bloc/character_bloc.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -9,7 +11,14 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   @override
+  void initState() {
+    context
+        .read<CharacterBloc>()
+        .add(const CharacterEvent.fetch(name: '', page: 1));
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
