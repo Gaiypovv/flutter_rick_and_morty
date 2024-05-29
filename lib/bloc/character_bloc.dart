93f10648cc5beb9db8cfbd00a24c27d8620033bc
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 12:28:05 +0600

    реализовал character_bloc.freezed

--- lib/bloc/character_bloc.dart
+++ lib/bloc/character_bloc.dart
@@ -2,5 +2,5 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'character_bloc.freezed.dart';
 part 'character_bloc.g.dart';
-part 'character_event.dart'
-part 'character_state.dart'
+part 'character_event.dart';
+part 'character_state.dart';
---
+++ lib/bloc/character_bloc.freezed.dart
@@ -0,0 +1,239 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'character_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$CharacterEvent {
+  String get name => throw _privateConstructorUsedError;
+  int get page => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(String name, int page) fetch,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function(String name, int page)? fetch,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(String name, int page)? fetch,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(CharacterEventFetch value) fetch,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(CharacterEventFetch value)? fetch,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(CharacterEventFetch value)? fetch,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CharacterEventCopyWith<CharacterEvent> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CharacterEventCopyWith<$Res> {
+  factory $CharacterEventCopyWith(
+          CharacterEvent value, $Res Function(CharacterEvent) then) =
+      _$CharacterEventCopyWithImpl<$Res, CharacterEvent>;
+  @useResult
+  $Res call({String name, int page});
+}
+
+/// @nodoc
+class _$CharacterEventCopyWithImpl<$Res, $Val extends CharacterEvent>
+    implements $CharacterEventCopyWith<$Res> {
+  _$CharacterEventCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? page = null,
+  }) {
+    return _then(_value.copyWith(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      page: null == page
+          ? _value.page
+          : page // ignore: cast_nullable_to_non_nullable
+              as int,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$CharacterEventFetchImplCopyWith<$Res>
+    implements $CharacterEventCopyWith<$Res> {
+  factory _$$CharacterEventFetchImplCopyWith(_$CharacterEventFetchImpl value,
+          $Res Function(_$CharacterEventFetchImpl) then) =
+      __$$CharacterEventFetchImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String name, int page});
+}
+
+/// @nodoc
+class __$$CharacterEventFetchImplCopyWithImpl<$Res>
+    extends _$CharacterEventCopyWithImpl<$Res, _$CharacterEventFetchImpl>
+    implements _$$CharacterEventFetchImplCopyWith<$Res> {
+  __$$CharacterEventFetchImplCopyWithImpl(_$CharacterEventFetchImpl _value,
+      $Res Function(_$CharacterEventFetchImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? page = null,
+  }) {
+    return _then(_$CharacterEventFetchImpl(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      page: null == page
+          ? _value.page
+          : page // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$CharacterEventFetchImpl implements CharacterEventFetch {
+  const _$CharacterEventFetchImpl({required this.name, required this.page});
+
+  @override
+  final String name;
+  @override
+  final int page;
+
+  @override
+  String toString() {
+    return 'CharacterEvent.fetch(name: $name, page: $page)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$CharacterEventFetchImpl &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.page, page) || other.page == page));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, name, page);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$CharacterEventFetchImplCopyWith<_$CharacterEventFetchImpl> get copyWith =>
+      __$$CharacterEventFetchImplCopyWithImpl<_$CharacterEventFetchImpl>(
+          this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(String name, int page) fetch,
+  }) {
+    return fetch(name, page);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function(String name, int page)? fetch,
+  }) {
+    return fetch?.call(name, page);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(String name, int page)? fetch,
+    required TResult orElse(),
+  }) {
+    if (fetch != null) {
+      return fetch(name, page);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(CharacterEventFetch value) fetch,
+  }) {
+    return fetch(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(CharacterEventFetch value)? fetch,
+  }) {
+    return fetch?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(CharacterEventFetch value)? fetch,
+    required TResult orElse(),
+  }) {
+    if (fetch != null) {
+      return fetch(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class CharacterEventFetch implements CharacterEvent {
+  const factory CharacterEventFetch(
+      {required final String name,
+      required final int page}) = _$CharacterEventFetchImpl;
+
+  @override
+  String get name;
+  @override
+  int get page;
+  @override
+  @JsonKey(ignore: true)
+  _$$CharacterEventFetchImplCopyWith<_$CharacterEventFetchImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
