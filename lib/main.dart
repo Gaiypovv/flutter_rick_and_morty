3ccbed7d89d83e1b9c48beb23b69114e2c64466b
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-29 14:00:09 +0600

    Указал шрифт для общего приложения

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.light,
         primaryColor: Colors.black,
+        fontFamily: 'Georgia',
       ),
       home: HomePage(title: 'Rick and Morty'),
     );
