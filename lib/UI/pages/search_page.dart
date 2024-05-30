9728b43f0f40105fb5d8341aa06b9f58bc6ad24b
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-05-30 11:02:32 +0600

    Реализовал состояние error

--- lib/UI/pages/search_page.dart
+++ lib/UI/pages/search_page.dart
@@ -22,19 +22,20 @@ class _SearchPageState extends State<SearchPage> {
     final state = context.watch<CharacterBloc>().state;
 
     return state.when(
-        loading: () {
-          return const Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircularProgressIndicator(strokeWidth: 2),
-                SizedBox(width: 10),
-                Text('Loading...'),
-              ],
-            ),
-          );
-        },
-        loaded: loaded,
-        error: error);
+      loading: () {
+        return const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(strokeWidth: 2),
+              SizedBox(width: 10),
+              Text('Loading...'),
+            ],
+          ),
+        );
+      },
+      loaded: loaded,
+      error: () => const Text('Nothing found...'),
+    );
   }
 }
